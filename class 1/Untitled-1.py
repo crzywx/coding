b8af642391ccd291c7f1b3edc0273d75eb405820
solezn <topgbelike@gmail.com>
2026-06-07 16:36:27 +0100

    class2

---
+++ class 1/Untitled-1.py
@@ -0,0 +1,10 @@
+print("My Daily routine")
+
+print("I go to school at 8am")
+
+print("I play with my friends in the evenings.")
+
+print("I study for 2 hours at night.")
+
+print("Total study hours in a week:", 2 * 7)
+

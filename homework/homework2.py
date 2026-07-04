aa643736b344ae6e1a87d8b06683a6a733722c39
solezn <topgbelike@gmail.com>
2026-07-04 10:52:17 +0100

    e

---
+++ homework/homework2.py
@@ -0,0 +1,9 @@
+temp = (int(input("What is the temperature outside?")))
+
+if temp <= 10:
+    print("Wear a thick jacket, it's cold")
+elif temp <= 20:
+    print("It's quite cool, wear a light jacket")
+elif temp <= 30:
+    print("No jacket needed, very hot")
+

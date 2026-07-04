d007f6963dcf574aeff1802d7ba6bbd09aa418e2
solezn <topgbelike@gmail.com>
2026-07-04 11:43:02 +0100

    e

---
+++ class 6/activity1.py
@@ -0,0 +1,18 @@
+v = 3
+w = 2
+x = 9
+y = 3
+z = (v+w) * x / y 
+print(z)
+
+
+name = "Shane"
+age = 14
+#true or false and false
+#true or false
+#true
+if name == "Shane" or name == "Shawn" and age >= 15:
+    print("Welcome!")
+else:
+    print("Good Bye!")
+
---
+++ class 6/activity2.py
@@ -0,0 +1,8 @@
+numerator = int(input("Enter a numerator"))
+denominator = int(input("Enter a denominator"))
+
+if numerator%denominator == 0:
+    print("Divisible")
+else:
+    print("Not Divisible")
+

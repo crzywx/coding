f507df77d49033cea36d4257716f6daeaa7c2e60
solezn <topgbelike@gmail.com>
2026-08-02 17:40:31 +0100

    1

---
+++ class 9/activity2.py
@@ -0,0 +1,6 @@
+name = input("Enter a name ")
+
+name2=""
+for letter in name:
+    name2=letter+name2
+print("Reversed string =" ,name2)
---
+++ class 9/activity3.py
@@ -0,0 +1,4 @@
+n = int(input("Enter a number to count down from"))
+
+for i in range(n,0,-1):
+    print(i)

40e72c06f9c28c83093960c7b67e462354add306
solezn <topgbelike@gmail.com>
2026-08-22 12:31:11 +0100

    c

--- class12/activity3.py
+++ class12/activity3.py
@@ -14,4 +14,13 @@ for i in range(1,halfdiamondrow+1):
         print(end=str(num))
         num += 1
     print()
-
+space = 1
+for i in range(1 , halfdiamondrow):
+        for j in range(1, space+1):
+            print(end=" ")
+        space = space+1
+        num = 1 
+        for j in range(1, 2*(halfdiamondrow-i)):
+             print(end=str(num))
+             num = num+1
+        print()
